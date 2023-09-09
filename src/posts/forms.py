9c502ec606ec16a476f5058649b819771277de94
Im Lain <lydie.chauveau@live.com>
2023-09-09 20:14:05 +0200

    add multiple images in one commit

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from .models import Pictures
-
 from django.forms import modelformset_factory
 
+class MultipleFileInput(forms.FileInput):
+    def render(self, name, value, attrs=None, renderer=None):
+        if 'multiple' not in self.attrs:
+            self.attrs['multiple'] = 'multiple'
+        return super().render(name, value, attrs, renderer)
 
 class PictureForm(forms.ModelForm):
+    img = forms.FileField(widget=MultipleFileInput(), required=False)
+
     class Meta:
         model = Pictures
         fields = ['img']
+
 PictureFormSet = modelformset_factory(Pictures, form=PictureForm, extra=1)
