0cdb092c43b6ae2ca464e3bf362b4ba46843fdb1
Im Lain <lydie.chauveau@live.com>
2023-09-10 17:29:36 +0200

    signup page success

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -7,19 +7,10 @@ from django.urls import path, include
 app_name = "posts"
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    # User-related URLs
-    #path('signup/', SignUpView.as_view(), name='signup'),
-    # path("accounts/", include("django.contrib.auth.urls")),
     path('create/', BlogPostCreate.as_view(), name="create"),
-
-    path('<str:username>/', BlogHome.as_view(), name="profile"),
     path('search/', UserSearchView.as_view(), name="user_search"),
-
-    # Blog post-related URLs
+    path('<str:username>/', BlogHome.as_view(), name="profile"),
     path('<str:username>/<str:slug>/', BlogPostDetail.as_view(), name="post"),
     path('edit/<str:username>/<str:slug>/', BlogPostEdit.as_view(), name="edit"),
     path('delete/<str:username>/<str:slug>/', BlogPostDelete.as_view(), name="delete"),
-
-
 ]
